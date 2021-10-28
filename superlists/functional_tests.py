2a333d61fe041668b402454e7ee0f9b3c5171531
Dmaina5054 <dmain5054@gmail.com>
2021-10-28 22:25:21 +0300

    first FT in comments Changes done to use unittests

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,21 +1,39 @@
-
 from selenium import webdriver
-browser = webdriver.Firefox()
+import unittest
+# browser = webdriver.Firefox()
 
 
-#user go to homepage to checkout app
-browser.get('http://localhost:8000')
+# #user go to homepage to checkout app
+# browser.get('http://localhost:8000')
 
-#notice the page title and header mention todo list
+# #notice the page title and header mention todo list
 
-assert 'To-Do' in browser.title
-#invited to enter a to do list
-#type "Buy fancy leather jacket"
-#press enter and show list view
-#1. buy a fancy leather jacket
-#still another text box available to add more items
-#types "Attend Men conference"
-#page updates now two items in list
-#satisfied go to sleep
+# assert 'To-Do' in browser.title
+# #invited to enter a to do list
+# #type "Buy fancy leather jacket"
+# #press enter and show list view
+# #1. buy a fancy leather jacket
+# #still another text box available to add more items
+# #types "Attend Men conference"
+# #page updates now two items in list
+# #satisfied go to sleep
 
-browser.quit()
+# browser.quit()
+
+#ADOPT standard unitests python module
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox() 
+    def tearDown(self):
+        self.browser.quit()
+    #this is logic to test
+    def test_can_start_a_list_and_retreive_later(self):
+        #User check out new heard todolist app-Homepage
+        self.browser.get('http://localhost:8000')
+        
+        #user notices page titke and header with to-do list
+        self.assertIn('To-Do',self.browser.title)
+        self.fail('Finish the test')
+        
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
