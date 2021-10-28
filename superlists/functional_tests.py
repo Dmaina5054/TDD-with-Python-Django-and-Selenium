374ce4aece0ee2570b828f19788a01eaea85c579
Dmaina5054 <dmain5054@gmail.com>
2021-10-28 22:07:31 +0300

    initial

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,6 +1,21 @@
 
 from selenium import webdriver
 browser = webdriver.Firefox()
+
+
+#user go to homepage to checkout app
 browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+#notice the page title and header mention todo list
+
+assert 'To-Do' in browser.title
+#invited to enter a to do list
+#type "Buy fancy leather jacket"
+#press enter and show list view
+#1. buy a fancy leather jacket
+#still another text box available to add more items
+#types "Attend Men conference"
+#page updates now two items in list
+#satisfied go to sleep
+
+browser.quit()
